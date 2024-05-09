b4b4daaba5aea1358875efe3a93ffe49bc854e2e
caio.araujo <caio.araujo@edu.pucrs.br>
2024-05-09 13:38:31 -0300

    feat: translate getData message

--- scripts/getData.js
+++ scripts/getData.js
@@ -5,6 +5,6 @@ export default function getData(endpoint) {
     return fetch(`${baseURL}${endpoint}`)
         .then(response => response.json())
         .catch(error => {
-            console.error('Erro ao acessar o endpoint /series/top5:', error);
+            console.error(`Error when accessing the endpoint ${endpoint}`, error);
         });
 }
